e41cec1a09a24c29669d129ff5a4c71d2bd16733
Louis Gérard <louis.gerard@isen-lille.fr>
2016-10-19 15:31:18 -0400

    zbuli

--- Algo_Devoir2/Exercice2/TwoThreeFourTree.h
+++ Algo_Devoir2/Exercice2/TwoThreeFourTree.h
@@ -12,10 +12,9 @@ public:
 	bool isEmpty() const;
 	void insertValue(const T &value, TwoThreeFourNode<T> currentNode);
 	void removeValue(const T &);
-	const T & max() const;
-	const T & min() const;
 	int getNbNodes() const;
 	TwoThreeFourNode<T> search(const T &) const;
+	int getNextNodeToVisit(T value, TwoThreeFourNode<T> currentNode);
 };
 
 template<typename T>
@@ -75,27 +74,7 @@ inline void TwoThreeFourTree<T>::insertValue(const T & value, TwoThreeFourNode<T
 		}
 		//Find the child whose interval contains the value to be inserted
 		TwoThreeFourNode<T> childFound;
-		//If this is a 2 node
-		if (currentNode.getChildren().size() == 2) {
-			if (value < currentNode.getKeys()[0]) {
-				childFound = currentNode.getChildren()[0];
-			}
-			else if (value > currentNode.getKeys()[0]){
-				childFound = currentNode.getChildren()[1];
-			}
-		}
-		//If this is a 3 node
-		else if (currentNode.getChildren().size() == 3) {
-			if (value < currentNode.getKeys()[0]) {
-				childFound = currentNode.getChildren()[0];
-			}
-			else if (value > currentNode.getKeys()[1]){
-				childFound = currentNode.getChildren()[2];
-			}
-			else if(value < currentNode.getKeys()[0] && value > currentNode.getKeys()[1]){
-				childFound = currentNode.getChildren()[1];
-			}
-		}
+		childFound = currentNode.getChildren()[getNextNodeToVisit(value,currentNode)]
 		if (childFound.isLeaf()) {
 			childFound.getKeys().insert(value);
 		}
@@ -108,20 +87,43 @@ inline void TwoThreeFourTree<T>::insertValue(const T & value, TwoThreeFourNode<T
 template<typename T>
 inline void TwoThreeFourTree<T>::removeValue(const T & value)
 {
-	//C'est chaud aussi
+	TwoThreeFourNode<T> currentNode;
+	TwoThreeFourNode<T> nextNode = this->root;
+	bool found = false;
+	while (!found) {
+		currentNode = nextNode;	
+		if (!currentNode.isRoot()) {
+			int i = 0;
+			for (T element : currentNode.getKeys()) {
+				if element == value() {
+					found = true;
+					currentNode.getKeys().erase(i);
+				}
+				i++;
+			}
+		}
+		int indexNodeToVisit = getNextNodeToVisit(value, currentNode)
+		nextNode = currentNode.getChildren()[indexNodeToVisit];
+		nearestSibling = currentNode.getChildren()[indexNodeToVisit + 1];
+		//If nextNode is not a 2 node 
+		if (nextNode.is2Node()) {
+			if (nearestSibling.is2Node()) {
+				if (currentNode.is2Node()) { //currentNode must be root
+
+				}
+				else {
+
+				}
+			}
+			else if (nearestSibling.is3Node() || nearestSibling.is4Node()) {
+				T tmp = currentNode.getKeys()[0];
+				currentNode.getKeys()[0] = nearestSibling.getKeys()[0];
+				nextNode.getKeys().insert(tmp)
+			}
+		}
+	}
 }
 
-template<typename T>
-inline const T & TwoThreeFourTree<T>::max() const
-{
-	// TODO: insert return statement here
-}
-
-template<typename T>
-inline const T & TwoThreeFourTree<T>::min() const
-{
-	// TODO: insert return statement here
-}
 
 template<typename T>
 inline int TwoThreeFourTree<T>::getNbNodes() const
@@ -144,45 +146,13 @@ inline TwoThreeFourNode<T> TwoThreeFourTree<T>::search(const T & value) const
 		}
 		if (!found) {
 			//Case of a Leaf
-			if (currentNode.isLeaf) {
+			if (currentNode.isLeaf()) {
 				return NULL;
 			}
-			//Two node
-			else if (currentNode.getChildren().size() == 2) {
-				if (value < currentNode.getKeys()[0]) {
-					nextNode = currentNode.getChildren()[0];
-				}
-				else if (value > currentNode.getKeys()[0]){
-					nextNode = currentNode.getChildren()[1];
-				}
+			else {
+				nextNode = currentNode.getChildren()[getNextNodeToVisit(value,currentNode)];
 			}
-			//Three node
-			else if (currentNode.getChildren().size() == 3) {
-				if (value < currentNode.getKeys()[0]) {
-					nextNode = currentNode.getChildren()[0];
-				}
-				else if (value > currentNode.getKeys()[1]) {
-					nextNode = currentNode.getChildren()[2];
-				}
-				else if (value > currentNode.getKeys()[0] && value < currentNode.getKeys()[1]) {
-					nextNode = currentNode.getChildren()[1];
-				}
-			}
-			//Four node
-			else if (currentNode.getChildren().size() == 4) {
-				if (value < currentNode.getKeys()[0]) {
-					nextNode = currentNode.getChildren()[0];
-				}
-				else if (value > currentNode.getKeys()[2]) {
-					nextNode = currentNode.getChildren()[3];
-				}
-				else if (value > currentNode.getKeys()[0] && value < currentNode.getKeys()[1]) {
-					nextNode = currentNode.getChildren()[1];
-				}
-				else if (value > currentNode.getKeys()[1] && value < currentNode.getKeys()[2]) {
-					nextNode = currentNode.getChildren()[2];
-				}
-			}
+			
 		}
 		else {
 			return currentNode;
@@ -190,4 +160,47 @@ inline TwoThreeFourNode<T> TwoThreeFourTree<T>::search(const T & value) const
 			
 	}
 	return false;
+
+
+}
+
+template<typename T>
+inline int TwoThreeFourTree<T>::getNextNodeToVisit(T value, TwoThreeFourNode<T> currentNode)
+{
+	//Two node
+			if (currentNode.getChildren().size() == 2) {
+				if (value < currentNode.getKeys()[0]) {
+					return 0;
+				}
+				else if (value > currentNode.getKeys()[0]) {
+					return 1;
+				}
+			}
+			//Three node
+			else if (currentNode.getChildren().size() == 3) {
+				if (value < currentNode.getKeys()[0]) {
+					return 0;
+				}
+				else if (value > currentNode.getKeys()[1]) {
+					return 2;
+				}
+				else if (value > currentNode.getKeys()[0] && value < currentNode.getKeys()[1]) {
+					return 1;
+				}
+			}
+			//Four node
+			else if (currentNode.getChildren().size() == 4) {
+				if (value < currentNode.getKeys()[0]) {
+					return 0;
+				}
+				else if (value > currentNode.getKeys()[2]) {
+					return 3;
+				}
+				else if (value > currentNode.getKeys()[0] && value < currentNode.getKeys()[1]) {
+					return 1;
+				}
+				else if (value > currentNode.getKeys()[1] && value < currentNode.getKeys()[2]) {
+					return 2;
+				}
+			}
 }
